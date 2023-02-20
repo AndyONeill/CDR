25fd5b34845de59b11466ec5ea5ccde955b8b2e8
Andrew <andyo.dev@gmail.com>
2023-02-20 17:11:19 +0000

    Specific integration test on test version of database

--- CDR.Integration.Tests/CallerControllerTests.cs
+++ CDR.Integration.Tests/CallerControllerTests.cs
@@ -1,29 +1,56 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using CDR.Data.models;
+using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http.Json;
 
 namespace CDR.Integration.Tests
 {
     public class CallerControllerTests : IClassFixture<WebApplicationFactory<Program>>
     {
         private readonly WebApplicationFactory<Program> _factory;
+        private readonly string spentByDayUrl = "/api/Caller/SpentByDay";
         public CallerControllerTests(WebApplicationFactory<Program> factory)
         {
-            _factory = factory;
+            var projectDir = Directory.GetCurrentDirectory();
+            var configPath = Path.Combine(projectDir, "appsettings.json");
+
+            _factory = factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureAppConfiguration((context, conf) =>
+                {
+                    conf.AddJsonFile(configPath);
+                });
+            });
         }
 
         [Fact]
-        public async Task GET_retrieves_weather_forecast()
+        public async Task GET_Caller_SpentByDay_Call_Success()
         {
             using var client = _factory.CreateClient();
-            var response = await client.GetAsync("/api/Caller/SpentByDay?callerId=441215598896&from=01%2F01%2F2000&to=01%2F01%2F2025");
+            var exampleCallUrl = spentByDayUrl + "?callerId=441215598896&from=01%2F01%2F2000&to=01%2F01%2F2025";
+            var response = await client.GetAsync(exampleCallUrl);
             response.EnsureSuccessStatusCode();
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GET_Caller_SpentByDay_Specific()
+        {
+            using var client = _factory.CreateClient();
+            var exampleCallUrl = spentByDayUrl + "?callerId=441215598896&from=01%2F01%2F2000&to=01%2F01%2F2025";
+            var response = await client.GetAsync(exampleCallUrl);
+            var expected = new List<CallerSpend>
+            {
+                new CallerSpend{ Day = new DateTime(2016, 8,16), AverageCost=1.234, NumberCalls=1, TotalCost=1.234 }
+            };
+            var result = await response.Content.ReadFromJsonAsync<List<CallerSpend>>();
+            expected.Should().BeEquivalentTo(result);
+            Assert.Equal(expected, result);
+
+
         }
     }
 }
