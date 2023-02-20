6ba4e552de9aa56938ef52321b997d3b10189da5
Andrew <andyo.dev@gmail.com>
2023-02-20 15:51:42 +0000

    Add integration test project

---
+++ CDR.Integration.Tests/CallerControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDR.Integration.Tests
+{
+    public class CallerControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        public CallerControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GET_retrieves_weather_forecast()
+        {
+            using var client = _factory.CreateClient();
+            var response = await client.GetAsync("/api/Caller/SpentByDay?callerId=441215598896&from=01%2F01%2F2000&to=01%2F01%2F2025");
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
