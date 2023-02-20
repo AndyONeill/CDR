17a8899434ea76007ae16d90d49bff0286ad2e1e
Andrew <andyo.dev@gmail.com>
2023-02-20 11:53:49 +0000

    Sketch out first controller and projects

---
+++ CDR.Data/models/CallerSpend.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDR.Data.models
+{
+    public class CallerSpend
+    {
+        public DateTime Day { get; set; }
+        public int NumberCalls { get; set; }
+        public Double AverageCost { get; set; }
+        public Double TotalCost { get; set; }
+    }
+}
---
+++ CDR/Controllers/CallerController.cs
@@ -0,0 +1,28 @@
+﻿using CDR.Data.models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Experimental.ProjectCache;
+
+namespace CDR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CallerController : ControllerBase
+    {
+
+        private readonly ILogger<CallerController> _logger;
+
+        public CallerController(ILogger<CallerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("SpentByDay")]
+        public IEnumerable<CallerSpend> Get(DateTime from, DateTime To)
+        {
+            var result = new List<CallerSpend>();
+            result.Add(new CallerSpend{ Day = DateTime.Now, AverageCost = 32.123, NumberCalls=3, TotalCost = 7654.456 });
+            return result;
+        }
+    }
+}
